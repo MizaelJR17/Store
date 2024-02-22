21f1e97af2f839e3e73f1bcd0080b5d670750fc6
MizaelJR17 <mizael.jxjunior@aluno.educacao.pe.gov.br]>
2024-02-21 21:10:42 -0300

    Ajustando css

--- store/src/Sidebar.jsx
+++ store/src/Sidebar.jsx
@@ -1,50 +1,53 @@
-import React from 'react';
-import { BsCart3, BsGrid1X2Fill, 
-    BsFillArchiveFill, BsFillGrid3X3GapFill,
-     BsListCheck, BsMenuButtonWideFill, BsFillGearFill, BsCart } from 'react-icons/bs';
+import React from 'react'
+import 
+{BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, 
+  BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
+ from 'react-icons/bs'
 
-
-function Sidebar() {
+function Sidebar({openSidebarToggle, OpenSidebar}) {
   return (
-    <aside id ="sidebar">
+    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
         <div className='sidebar-title'>
-            <BsCart className='icon_header'/> SHOP
+            <div className='sidebar-brand'>
+                <BsCart3  className='icon_header'/> SHOP
+            </div>
+            <span className='icon close_icon' onClick={OpenSidebar}>X</span>
         </div>
-        <span className='icon close_icon'>X</span>
+
         <ul className='sidebar-list'>
             <li className='sidebar-list-item'>
                 <a href="">
-                      <BsCart3 className='icon'/> Dashboard   
+                    <BsGrid1X2Fill className='icon'/> Dashboard
                 </a>
             </li>
             <li className='sidebar-list-item'>
                 <a href="">
-                      <BsGrid1X2Fill className='icon'/> Products   
+                    <BsFillArchiveFill className='icon'/> Products
                 </a>
             </li>
             <li className='sidebar-list-item'>
                 <a href="">
-                      <BsFillArchiveFill className='icon'/> Categories  
+                    <BsFillGrid3X3GapFill className='icon'/> Categories
                 </a>
             </li>
             <li className='sidebar-list-item'>
                 <a href="">
-                      <BsFillGrid3X3GapFill className='icon'/> Customers  
+                    <BsPeopleFill className='icon'/> Customers
                 </a>
             </li>
             <li className='sidebar-list-item'>
                 <a href="">
-                      <BsListCheck className='icon'/>  Inventory
+                    <BsListCheck className='icon'/> Inventory
                 </a>
             </li>
             <li className='sidebar-list-item'>
                 <a href="">
-                      <BsMenuButtonWideFill className='icon'/> Reports  
+                    <BsMenuButtonWideFill className='icon'/> Reports
                 </a>
             </li>
             <li className='sidebar-list-item'>
                 <a href="">
-                      <BsFillGearFill className='icon'/> Settings  
+                    <BsFillGearFill className='icon'/> Setting
                 </a>
             </li>
         </ul>
@@ -53,3 +56,4 @@ function Sidebar() {
 }
 
 export default Sidebar
+
